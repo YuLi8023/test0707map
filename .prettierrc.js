9f7afbbd84f0cad44b7f611465462260c65af6ec
YuLi8023 <993259583@qq.com>
2022-07-08 11:24:41 +0800

    增加vs code

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
     semi: true,
-    singleQuote:true,
-    trailingComma:'all',
-    arrowParens:'always',
-    printWidth:120,
-    tabWidth:4,
+    singleQuote: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    printWidth: 120,
+    tabWidth: 4,
 };
